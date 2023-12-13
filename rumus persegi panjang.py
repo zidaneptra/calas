d1e8fec7518db1674cff9f5aee337eae6199acb2
zidaneptra <153588312+zidaneptra@users.noreply.github.com>
2023-12-13 15:03:52 +0700

    Add files via upload

---
+++ rumus persegi panjang.py
@@ -0,0 +1,10 @@
+panjang = int(input("Masukkan panjang persegi panjang: "))
+lebar = int(input("Masukkan lebar persegi panjang: "))
+
+
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+
+
+print("Luas persegi panjang dengan panjang", panjang,"dan lebar", lebar, "adalah", luas)
+print("Keliling persegi panjang dengan panjang", panjang, "dan lebar", lebar, "adalah" ,keliling)
